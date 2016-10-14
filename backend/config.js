4001bc94b3c4ba2fe2b00bd7659e0e8d461a67c9
Cat Tien Nguyen <Cattien@Cats-MacBook-Air.local>
2016-10-14 00:30:03 -0400

    Initial commit

--- backend/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  database: 'mongodb://guestuser:guestpassword@ds017636.mlab.com:17636/authentication-server',
-  secret: 'babydonghua',
-  sendgridUser: 'marcushurney',
-  sendgridPassword: 'shandonghuapp2'
-};
