0cd43fd24548ba6c7cea667c2e30c71f02bf13e9
Marcus Hurney <marcushurney@gmail.com>
2016-10-14 23:33:32 -0400

    Removes console.log statement

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -2,7 +2,6 @@ const { User } = require('./../models/user');
 
 const authenticate = (req, res, next) => {
   // grab token from request object
-  console.log('HERE IS THE TOKEN', req.header('x-auth'));
   const token = req.header('x-auth');
 
   User.findByToken(token).then(user => {
