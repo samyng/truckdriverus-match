66f0a1a2cfcab35183c26caa8e25d4a7079c00bd
Cat Tien Nguyen <Cattien@Cats-MacBook-Air.local>
2016-10-14 21:51:50 -0400

    Changes '/forgotPassword' route to use ES6 promises instead of the async waterfall function.

--- backend/router.js
+++ backend/router.js
@@ -67,73 +67,155 @@ module.exports = function (app) {
 
 	// forgot password route, assigns user a reset token and sends email
 	// email will contain a link to the '/reset/:token' route below
-	app.post('/forgotPassword', function(req, res, next) {
+	// app.post('/forgotPassword', function(req, res, next) {
+	//
+	// 	waterfall([
+	// 		// generate reset token
+	// 		function(done) {
+	// 			crypto.randomBytes(20, function(err, buf) {
+  //       	var token = buf.toString('hex');
+  //       	done(err, token);
+  //     	});
+	// 		},
+	// 		function(token, done) {
+	// 			// search for user with the given email
+	// 			User.findOne({ email: req.body.email }, function(err, user) {
+	// 				// check to see if the user exists
+	// 				if (!user) {
+	// 					// user doesn't exist in database
+	// 					return res.status(404).send();
+	// 				}
+	// 				// user exists, assign token with expiration date
+	// 				user.resetPasswordToken = token;
+	// 				user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+	//
+	// 				// save the user model with the newly added
+	// 				// token and expiration date
+	// 				user.save(function(err) {
+	// 					done(err, token, user);
+	// 				});
+	// 			});
+  //   	},
+	// 		function(token, user, done) {
+	// 		    var smtpTransport = nodemailer.createTransport('SMTP', {
+	// 		      service: 'SendGrid',
+	// 		      auth: {
+	// 		        user: config.sendgridUser,
+	// 		        pass: config.sendgridPassword
+	// 		      }
+	// 		    });
+	//
+	// 		    var mailOptions = {
+	// 		      to: user.email,
+	// 		      from: 'mjuice@uga.edu',
+	// 		      subject: 'Password Reset',
+	// 		      text: `Hello,
+	// 								 We received a request to reset your password.
+	// 								 \n
+	// 								 To start the process, please click the following link:
+	// 								 http://${req.headers.host}/reset-password/${token}
+	// 								 \n
+	// 								 If the above link doesn’t take you to our password reset page,
+	// 								 copy and paste the URL into the search bar of a new browser window.
+	// 								 \n
+	// 								 The URL will expire in 1 hour for security reasons.
+	// 								 If you didn’t make this request, simply ignore this message.`
+	//
+	// 		    };
+	//
+	// 				// send the email using the mailOptions object defined above
+	// 		  	smtpTransport.sendMail(mailOptions, function(err) {
+	// 		    	done(err, 'done');
+	// 		  	});
+	//     	}],
+	// 			function(err) {
+	// 				// handle error
+	// 				if (err) return next(err);
+	// 				res.status(200).send();
+	// 			});
+	// 	}); // end POST route '/forgotPassword'
 
-		waterfall([
-			// generate reset token
-			function(done) {
-				crypto.randomBytes(20, function(err, buf) {
-        	var token = buf.toString('hex');
-        	done(err, token);
-      	});
-			},
-			function(token, done) {
-				// search for user with the given email
+	app.post('/forgotPassword', (req, res, next) => {
+		return new Promise((resolve, reject) => {
+			crypto.randomBytes(20, function(err, buf) {
+				if (err) { return reject(err); }
+
+      	var token = buf.toString('hex');
+      	resolve(token);
+    	});
+		})
+		.then(token => {
+			return new Promise((resolve, reject) => {
 				User.findOne({ email: req.body.email }, function(err, user) {
-					// check to see if the user exists
-					if (!user) {
-						// user doesn't exist in database
-						return res.status(404).send();
-					}
-					// user exists, assign token with expiration date
-					user.resetPasswordToken = token;
-					user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
 
-					// save the user model with the newly added
-					// token and expiration date
-					user.save(function(err) {
-						done(err, token, user);
+					// if no user is returned, throw 404
+					if (!user) { return reject(404); }
+
+					// user exists, assign token with expiration date
+			  	user.resetPasswordToken = token;
+				 	user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+
+					// save user with new token property
+					user.save(err => {
+						if (err) { return reject(err); }
+
+						const userAndToken = {
+							user,
+							token
+						};
+						// pass on user and token
+						resolve({userAndToken});
 					});
 				});
-    	},
-			function(token, user, done) {
-			    var smtpTransport = nodemailer.createTransport('SMTP', {
+			});
+		})
+		.then(userAndToken => {
+
+			return new Promise((resolve, reject) => {
+				var smtpTransport = nodemailer.createTransport('SMTP',
+					{
 			      service: 'SendGrid',
 			      auth: {
 			        user: config.sendgridUser,
 			        pass: config.sendgridPassword
-			      }
-			    });
+			    }
+			  });
 
-			    var mailOptions = {
-			      to: user.email,
-			      from: 'mjuice@uga.edu',
-			      subject: 'Password Reset',
-			      text: `Hello,
-									 We received a request to reset your password.
-									 \n
-									 To start the process, please click the following link:
-									 http://${req.headers.host}/reset-password/${token}
-									 \n
-									 If the above link doesn’t take you to our password reset page,
-									 copy and paste the URL into the search bar of a new browser window.
-									 \n
-									 The URL will expire in 1 hour for security reasons.
-									 If you didn’t make this request, simply ignore this message.`
+		    var mailOptions = {
+		      to: userAndToken.user.email,
+		      from: 'mjuice@uga.edu',
+		      subject: 'Password Reset',
+		      text: `Hello,
+								 We received a request to reset your password.
+								 \n
+								 To start the process, please click the following link:
+								 http://${req.headers.host}/reset-password/${userAndToken.token}
+								 \n
+								 If the above link doesn’t take you to our password reset page,
+								 copy and paste the URL into the search bar of a new browser window.
+								 \n
+								 The URL will expire in 1 hour for security reasons.
+								 If you didn’t make this request, simply ignore this message.`
 
-			    };
+		    };
 
-					// send the email using the mailOptions object defined above
-			  	smtpTransport.sendMail(mailOptions, function(err) {
-			    	done(err, 'done');
-			  	});
-	    	}],
-				function(err) {
-					// handle error
-					if (err) return next(err);
-					res.status(200).send();
-				});
-		}); // end POST route '/forgotPassword'
+				// send the email using the mailOptions object defined above
+		  	smtpTransport.sendMail(mailOptions, function(err) {
+					if (err) { return reject(err); }
+		    	resolve();
+		  	});
+			});
+		})
+		.then(() => { res.status(200).send(); })
+		.catch(err => {
+			//check if the error is the one from the DB where the user was not found
+      if(err == 404) {
+        return res.status(404).send();
+      }
+			console.log(err);
+      return res.status(500).send(err);
+		});
+	}); // end POST '/forgotPassword'
 
 		// POST resetPassword route actually changes the user's password in the database
 		app.post('/resetPassword/:token', function(req, res) {
