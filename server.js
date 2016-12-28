46db525b0387f0c8d98471a665373fc98c0a24e4
Sam Yang <syangam@gmail.com>
2016-12-27 18:15:42 -0600

    change email message

--- server.js
+++ server.js
@@ -236,10 +236,8 @@ const sendEmail = (firstName, email, jobURL) => {
           value: `<html>
                     <p>Hi ${firstName},</p>
                     <p>
-                      My name is Tiffany. I work with Truck Driver US to connect drivers with jobs they're interested in. I'm happy to send you a new job link daily.
-                      <br/>
-                      Here is the one for today - are you interested?
-                      <a href=${jobURL}>Daily job link from Truck Driver US</a>
+                      My name is Tiffany. I work with Truck Driver US to connect drivers with jobs they're interested in. I'm happy to send you a new job link daily. Here is the one for today - are you interested?
+                      <br/><br/><a href=${jobURL}>Daily job link from Truck Driver US</a>
                     </p>
                     <br/>
                     <p>Thanks!</p>
