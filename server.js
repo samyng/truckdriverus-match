862ef2cf51c4ce8d8c99c2700e0e95a71ca3179f
Marcus Hurney <marcushurney@gmail.com>
2016-12-31 22:40:26 -0500

    Update server.js
    Handles edge case where candidate does not have an email address and/or phone number. Renames fetchClients to matchCandidates for a better description of function's functionality.

--- server.js
+++ server.js
@@ -58,15 +58,6 @@ var p = plivo.RestAPI({
   authToken: process.env.PLIVO_AUTH_TOKEN
 });
 
-// nodemailer configuration with sendgrid
-// var smtpTransport = nodemailer.createTransport('SMTP', {
-//   service: 'SendGrid',
-//   auth: {
-//     user: process.env.SEND_USER,
-//     pass: process.env.SEND_PASS
-//   }
-// });
-
 //Express App Setup
 
 app.use(morgan('combined')); //logs incoming requests
@@ -110,11 +101,12 @@ app.post('/updateCandidates', function(req, res, next) {
     });
 });
 
+const FEED_URL = 'http://api.jobs2careers.com/api/search.php?id=2538&pass=v9NloGlKCT8SwVeb&ip=2601:c0:c100:2bc:9902:4667:1173:86ed&q=&l=USA&industry=Trucking&format=json&limit=200';
 app.post('/sendSMS', function(req, res, next) {
   axios.get(FEED_URL)
     .then(data => {
       let jobs = data.data.jobs;
-      fetchClients(jobs, 'sms').then(() => {
+      matchCandidates(jobs, 'sms').then(() => {
         res.sendStatus(200);
       })
       .catch((err) => {
@@ -131,7 +123,7 @@ app.post('/sendEmails', function(req, res, next) {
   axios.get(FEED_URL)
     .then(data => {
       let jobs = data.data.jobs;
-      fetchClients(jobs, 'email').then(() => {
+      matchCandidates(jobs, 'email').then(() => {
         res.sendStatus(200);
       })
       .catch((err) => {
@@ -144,6 +136,42 @@ app.post('/sendEmails', function(req, res, next) {
     });
 });
 
+// LOAD CANDIDATES INTO DATABASE WITH readCSV
+const readCSV = () => {
+  return new Promise((resolve, reject) => {
+    //CSV File Path or CSV String or Readable Stream Object
+    var csvFileName="./public/uploads/candidates.csv";
+
+    //new converter instance
+    var csvConverter = new Converter({});
+
+    //end_parsed will be emitted once parsing finished
+    csvConverter.on("end_parsed",function(jsonObj){
+        jsonObj.forEach(person => {
+          const candidate = new Candidate();
+
+          candidate.firstName = person['First Name'];
+          candidate.lastName = person['Last Name'];
+          candidate.email = person['Email'];
+          candidate.location = person['Location'];
+          // extract state from location
+          candidate.state = extractState(person['Location']).trim();
+          // remove dashes from candidate's phone number before saving
+          if (person['Phone']) {
+            candidate.phone = person['Phone'].replace(/-/g, "");
+          }
+          candidate.save();
+        });
+        resolve();
+    });
+
+    //read from file
+    fs.createReadStream(csvFileName).pipe(csvConverter);
+  })
+  .catch((err) => {
+    reject(err);
+  });
+};
 
 // BEGIN HELPER FUNCTIONS --------------------------------------->
 // const extractCity = location => {
@@ -197,7 +225,14 @@ const selectJobsLessThanMax = (candidatesJobs, jobsSent, max) => {
   return jobsThatPass;
 };
 
-
+// returns empty string if firstName is undefined
+const checkFirstName = firstName => {
+  if (firstName !== undefined) {
+    return firstName;
+  } else {
+    return '';
+  }
+};
 
 let totalSMSSent = 0;
 
@@ -207,7 +242,14 @@ const sendPlivoSMS = (number, message) => {
       'dst' : `+1${number}`,
       'text' : message
     };
-
+    // if (number === '7064834776') {
+    //   p.send_message(params, function (status, response) {
+    //     totalSMSSent++;
+    //     console.log("Here are the total SMS sent: ", totalSMSSent);
+    //     console.log('Status: ', status);
+    //     console.log('API Response:\n', response);
+    //   });
+    // }
     p.send_message(params, function (status, response) {
       totalSMSSent++;
       console.log("Here are the total SMS sent: ", totalSMSSent);
@@ -303,7 +345,7 @@ const createCandidates = () => {
   // let candidate = new Candidate();
   // candidate.firstName = 'Kathy';
   // candidate.lastName = 'Nguyen';
-  // candidate.email = 'marcushurney@gmail.com';
+  // candidate.email = 'kathy@gmail.com';
   // candidate.state = 'MA';
   // // remove dashes from candidate's phone number before saving
   // candidate.phone = '4043940821';
@@ -335,160 +377,130 @@ const createCandidates = () => {
 
 
 
-const FEED_URL = 'http://api.jobs2careers.com/api/search.php?id=2538&pass=v9NloGlKCT8SwVeb&ip=2601:c0:c100:2bc:9902:4667:1173:86ed&q=&l=USA&industry=Trucking&format=json&limit=200';
 const PUBLISHER_ID = '2595';
 const MAX_MESSAGE_LIMIT = 50;
-let jobs = [];
-let matchingClients = [];
+let matchedCandidates = [];
 
-const fetchClients = (allJobs, typeOfReq) => {
+const matchCandidates = (allJobs, typeOfReq) => {
   return new Promise((resolve, reject) => {
     async.each(allJobs, function(job, callback) {
 
-      // for each city in the job.city array, add a space after the comma
-      // goes from: Atlanta,GA => Atlanta, GA
-      // let jobCities = job.city.map(city => {
-      //   return addSpaceAfterComma(city);
-      // });
       let jobStates = job.city.map(city => {
         return extractState(city);
       });
 
-      // change back to jobCities to search by city and state
       Candidate.find({ state: { "$in": jobStates }})
         .lean().exec(function(err, candidates) {
 
-          let newMatches = [];
+          let candidatesWithJobs = candidates.filter(candidate => {
 
-          candidates.forEach(candidate => {
-            // check if client already exists in matchingClients
-            let existingCandidate = {};
-            existingCandidate = _.findWhere(matchingClients, { phone: candidate.phone });
+            //make sure candidate has a number or email before adding
+            if (candidate.phone || candidate.email) {
+              // add the job to the candidate model
+              candidate.jobs = [];
+              candidate.jobs = [job];
+              return true;
+            } else {
+              return false;
+            }
 
-            // if the job does not contain uber or lyft and the price is high continue
+          });
+
+          // check to see if candidate has been matched before
+          candidatesWithJobs.forEach(candidateWithNewJob => {
+            // check to see if candidateWithNewJob already exists in matchedCandidates
+            let existingCandidate = undefined;
+
+            // check if the candidate in question has a phone number
+            if (candidateWithNewJob.phone) {
+              // check for existing candidate by phone num
+              existingCandidate = _.findWhere(matchedCandidates, { phone: candidateWithNewJob.phone });
+            } else if (candidateWithNewJob.email) {
+              // no phone number but has email address, so check for existing candidate by email
+              existingCandidate = _.findWhere(matchedCandidates, { email: candidateWithNewJob.email });
+            }
+
+            //if the job does not contain uber or lyft and the price is high continue
             if (!containsUberLyft(job.title) &&
                 priceIsHigh(job.price) &&
                 !containsInstaCart(job.description) &&
                 !containsPostmates(job.description) &&
                 !containsDeliv(job.title)){
-              // check to see if the matched candidate already exists
-              if (existingCandidate) {
-                // add job to existing job
-                existingCandidate.jobs = [ ...existingCandidate.jobs, job];
-              } else {
-                // this is a new matched candidate so create jobs array
-                candidate.jobs = [];
-                candidate.jobs = [job];
-                // push newly matched candidate onto newMatches array
-                newMatches = [ ...newMatches, candidate ];
-              }
-            } else {
-              return;
-            }
+
+                  if (existingCandidate !== undefined) {
+                    // the candidate has been matched before
+                    // so add the new job to the candidate's existing jobs array
+                    existingCandidate.jobs.push(candidateWithNewJob.jobs[0]);
+                  } else {
+                    // the candidate is new so add him/her to matchedCandidates
+                    matchedCandidates.push(candidateWithNewJob);
+                  }
+
+                } else {
+                  return;
+                }
           });
 
-          // push newMatches array onto matchingClients
-          matchingClients = [ ...matchingClients, ...newMatches ];
-
-          callback();
+          callback(); //async.each callback
         });
 
     }, function(err) {
-        if( err ) {
+        if (err) {
           console.log(err);
           reject(err);
         } else {
-          // pass all matchingClients with their associated jobs onward to sendJobs
-          resolve(sendJobs(matchingClients, typeOfReq));
+          // pass matchedCandidates with their associated jobs onward to sendJobs
+          resolve(sendJobs(matchedCandidates, typeOfReq));
         }
     });
-  });
+  }); //end Promise
 };
 
-let totalMessages = 0;
 const sendJobs = (candidatesArray, typeOfReq) => {
 
   return new Promise((resolve, reject) => {
+
     // array for tracking which jobs have been sent via sms
     let jobsSent = [];
 
     async.each(candidatesArray, function(candidate, callback) {
 
-      // find all jobs on candidate's job array that have been sent less than 50 times
-      let jobsUnderLimit = selectJobsLessThanMax(candidate.jobs, jobsSent, MAX_MESSAGE_LIMIT);
+      // find all jobs on candidate's job array that have been sent less than the max message limit
+      let jobsToSend = selectJobsLessThanMax(candidate.jobs, jobsSent, MAX_MESSAGE_LIMIT);
 
-      // only send a job if the array jobsUnderLimit is populated, otherwise do nothing
-      if (jobsUnderLimit.length) {
-        // send the first job in the passing jobs array for this candidate
-        let jobToSend = jobsUnderLimit[0];
+      if (jobsToSend.length) {
+          // construct jobURL with the first job in jobsToSend
+          let jobURL = `http://www.jobs2careers.com/click.php?id=${jobsToSend[0].id}.${PUBLISHER_ID}`;
 
-        // construct jobURL
-        let jobURL = `http://www.jobs2careers.com/click.php?id=${jobToSend.id}.${PUBLISHER_ID}`;
+            // check to see the type of request being sent
+            if (typeOfReq === 'sms') {
+              // check to see if candidate has a firstName property
+              let candidateFirstName = checkFirstName(candidate.firstName);
+              // construct sms message to send
+              let messageToSend =  `Hi ${candidateFirstName}! My name is Tiffany. I found your profile online and you look like a great fit for this role - are you interested? ${jobURL}`;
+              // send the actual SMS here
+              sendPlivoSMS(candidate.phone, messageToSend);
+            } else if (typeOfReq === 'email') {
+              // send the email here
+              sendEmail(candidate.firstName, candidate.email, jobURL);
+            }
 
-        // send message without bitly START
+            // JOB HAS BEEN SENT, NOW TRACK THE JOB
 
-        // make sure candidate.firstName is not undefined
-        let candidateFirstName;
-        if (candidate.firstName == undefined) {
-          // assign empty string so message looks natural;
-          candidateFirstName = '';
-        } else {
-          // candidate.firstName exists, so continue
-          candidateFirstName = candidate.firstName;
-        }
+            // 1: jobJustSent is the job that was just sent via SMS
+            let jobJustSent = { jobId: jobsToSend[0].id , sent: 1 };
 
-        // defines the content of the sms
-        let messageToSend =  `Hi ${candidateFirstName}! My name is Tiffany. I found your profile online and you look like a great fit for this role - are you interested? ${jobURL}`;
-        // console.log(`You sent a message to ${candidate.firstName} ${candidate.lastName}. He/She lives in ${candidate.state}`);
+            // 2: see if jobJustSent already exists in our tracking array (jobsSent)
+            let jobExists = _.findWhere(jobsSent, { jobId: jobJustSent.jobId });
 
-        if (typeOfReq === 'sms') {
-          // send the actual SMS here
-          sendPlivoSMS(candidate.phone, messageToSend);
-        } else if (typeOfReq === 'email') {
+            if (jobExists) {
+              // 3: if job already exists, increment the sent property
+              jobExists.sent++;
+            } else {
+              // 4: job does not exist so add it to jobsSent with a default sent = 1
+              jobsSent = [ ...jobsSent, jobJustSent];
+            }
 
-          sendEmail(candidate.firstName, candidate.email, jobURL);
-        }
-
-        // send message with BITLY START
-        // bitly.shorten(jobURL)
-        //   .then(function(response) {
-        //     console.log(response);
-        //     var short_url = response.data.url
-        //     // console.log(jobURL);
-        //     // console.log(short_url);
-        //     let messageToSend =  `Hi ${candidate.firstName}! My name is Tiffany. I found your profile online and you look like a great fit for this role - are you interested? ${short_url}`;
-        //     // console.log(`You sent a message to ${candidate.firstName} ${candidate.lastName}. He/She lives in ${candidate.state}`);
-        //
-        //     if (typeOfReq === 'sms') {
-        //       // send the actual SMS here
-        //
-        //       sendPlivoSMS(candidate.phone, messageToSend);
-        //     } else if (typeOfReq === 'email') {
-        //       // send the email
-        //       // if (candidate.email === 'marcushurney@gmail.com' || candidate.email === 'jennifer@gethappie.me') {
-        //       //   sendEmail(candidate.firstName, candidate.email, jobURL);
-        //       // }
-        //       sendEmail(candidate.firstName, candidate.email, jobURL);
-        //     }
-        //   }, function(error) {
-        //     throw error;
-        //   });
-
-        // JOB HAS BEEN SENT, NOW TRACK THE JOB
-
-        // 1: jobJustSent is the job that was just sent via SMS
-        let jobJustSent = { jobId: jobToSend.id , sent: 1 };
-
-        // 2: see if jobJustSent already exists in our tracking array (jobsSent)
-        let jobExists = _.findWhere(jobsSent, { jobId: jobJustSent.jobId });
-
-        if (jobExists) {
-          // 3: if job already exists, increment the sent property
-          jobExists.sent++;
-        } else {
-          // 4: job does not exist so add it to jobsSent with a default sent = 1
-          jobsSent = [ ...jobsSent, jobJustSent];
-        }
       }
 
       callback();
@@ -508,68 +520,5 @@ const sendJobs = (candidatesArray, typeOfReq) => {
         resolve();
       }
     });
-  });
-};
-
-
-const readCSV = () => {
-  return new Promise((resolve, reject) => {
-    //CSV File Path or CSV String or Readable Stream Object
-    var csvFileName="./public/uploads/candidates.csv";
-
-    //new converter instance
-    var csvConverter = new Converter({});
-
-    //end_parsed will be emitted once parsing finished
-    csvConverter.on("end_parsed",function(jsonObj){
-        jsonObj.forEach(person => {
-          // console.log(person);
-          // console.log(person.firstname);
-          // console.log(person.lastname);
-          // console.log(person.email);
-          // console.log(person.location);
-          // console.log(person.phone);
-          // this below works with Trucker Test File
-          // const candidate = new Candidate();
-          // candidate.firstName = person['firstname'];
-          // candidate.lastName = person['lastname'];
-          // candidate.email = person['email'];
-          // candidate.location = person['location'];
-          // candidate.state = extractState(person['location']).trim();
-          // // remove dashes from candidate's phone number before saving
-          // candidate.phone = person['phone'].replace(/-/g, "");
-          // candidate.save();
-
-          // original
-          // console.log(person['First Name']);
-          // console.log(person['Last Name']);
-          // console.log(person['Email']);
-          // console.log(person['Location']);
-          // if (person['Phone']) {
-          //   console.log(person['Phone']);
-          // }
-
-          const candidate = new Candidate();
-
-          candidate.firstName = person['First Name'];
-          candidate.lastName = person['Last Name'];
-          candidate.email = person['Email'];
-          candidate.location = person['Location'];
-          // extract state from location
-          candidate.state = extractState(person['Location']).trim();
-          // remove dashes from candidate's phone number before saving
-          if (person['Phone']) {
-            candidate.phone = person['Phone'].replace(/-/g, "");
-          }
-          candidate.save();
-        });
-        resolve();
-    });
-
-    //read from file
-    fs.createReadStream(csvFileName).pipe(csvConverter);
-  })
-  .catch((err) => {
-    reject(err);
   });
 };
